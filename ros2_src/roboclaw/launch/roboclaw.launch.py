72698181cf6a14164cd34bd18bd91b974ef3e68b
Veronica Diaz <60453038+Vernonicuhhh@users.noreply.github.com>
2026-01-29 23:55:43 -0800

    create roboclaw serial pkg

---
+++ ros2_src/roboclaw/launch/roboclaw.launch.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    
+    launch_joy_arg = DeclareLaunchArgument(
+        'launch_joy',
+        default_value='true',
+        description='Launch joy_node (set to False if already running)'
+    )
+    
+    port_arg = DeclareLaunchArgument(
+        'port',
+        default_value='/dev/ttyACM0',
+        description='Serial port for RoboClaw'
+    )
+    
+    baudrate_arg = DeclareLaunchArgument(
+        'baudrate',
+        default_value='38400',
+        description='Baudrate for serial communication'
+    )
+    
+    return LaunchDescription([
+        launch_joy_arg,
+        port_arg,
+        baudrate_arg,
+        
+        Node(
+            package='joy',
+            executable='joy_node',
+            name='joy_node',
+            parameters=[{
+                'device_id': 0, #TODO: fill out
+                'deadzone': 0.05,
+                'autorepeat_rate': 20.0,
+            }],
+            condition=IfCondition(LaunchConfiguration('launch_joy'))
+        ),
+        
+        Node(
+            package='simple_roboclaw',
+            executable='simple_roboclaw_joy',
+            name='simple_roboclaw_joy',
+            output='screen',
+            parameters=[{
+                'port': LaunchConfiguration('port'),
+                'baudrate': LaunchConfiguration('baudrate'),
+                'address': 128,
+                'max_speed': 100,
+                'axis_forward': 1,
+                'axis_turn': 0,
+            }]
+        ),
+    ])
